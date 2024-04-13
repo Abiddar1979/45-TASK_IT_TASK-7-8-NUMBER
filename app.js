0683ba88cb9a219f8e69f6c042c1a2d459e3d0c0
Abid Dar <abiddar1979@gmail.com>
2024-04-13 16:09:48 +0500

    files added

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Task #7 :Number Eight : write addition , subtraction , multiplication and Division operation that each result in th e number 8.Be sure to enclose your operation in print statement to see the result.
+
+Task # 8 : You should creat four lines that looks like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on the each line */
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(5 * 3);
+console.log(16 / 2);
